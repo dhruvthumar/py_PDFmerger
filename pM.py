851ca5fe53ed27163b69c4377461f750f4d90df7
Dhruv Thumar <dmthumar17@gmail.com>
2026-02-10 09:08:13 -0400

    guide: create an executable from the CLI as given in the code file at the end of the code

--- pM.py
+++ pM.py
@@ -1,4 +1,8 @@
-#MAKE gui to select pdf files and merge them
+
+# This code creates a simple GUI application using Tkinter that allows users 
+#        to select multiple PDF files and merge them into a single PDF file. 
+# The user can choose the output location and filename for the merged PDF.
+
 import tkinter as tk
 from tkinter import filedialog
 from PyPDF2 import PdfReader, PdfMerger
@@ -27,4 +31,10 @@ root.title("PDF Merger")
 merge_button = tk.Button(root, text="Merge PDFs", command=merge_pdfs)
 merge_button.pack(pady=100, padx=100)
 root.mainloop()
+
+# To create an executable file from this script using PyInstaller, 
+#     you can run the following command in your terminal or command prompt:
+# pyinstaller --onefile --windowed pM.py
+# This command will generate a single executable file without a console window. 
+#   The resulting executable will be found in the "dist" directory created by PyInstaller.
 
